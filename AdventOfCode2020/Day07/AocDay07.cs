b3c6d20dadc54149bafc40d889a5b06e9c05e8a2
Anders Andréen <anders.andreen@tele2.se>
2020-12-07 16:19:35 +0100

    Day 7 solved.

--- AdventOfCode2020/Day07/AocDay07.cs
+++ AdventOfCode2020/Day07/AocDay07.cs
@@ -10,20 +10,25 @@ namespace AdventOfCode2020.Day07
         public static void Run()
         {
             ConsoleOutput.Heading1("Advent of Code day 7");
-            // How many bag colors can eventually contain at least one shiny gold bag? 
             var bagRules = File
                 .ReadAllText(@"Day07\AocDay07.txt")
                 .Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
-                .Select(BagRule.Map)
+                .Select(Bag.Map)
                 .ToList();
-            var search = "shiny gold";
-            var x = bagRules.Where(rule =>
-                rule.Content.Any(item => item.Item2==search));
-            var resultStep1 = bagRules.Count();
-            Console.WriteLine($"Result part 1: {resultStep1}");
+            var searchTerm = "shiny gold";
 
-            var resultStep2 = 2;
-            Console.WriteLine($"Result part 2: {resultStep2}");
+            // https://adventofcode.com/2020/day/7#part1
+            //1: How many bag colors can eventually contain at least one shiny gold bag? 
+            var resultStep1 = bagRules
+                .SelectMany(rule=>rule.Match(bagRules, searchTerm))
+                .Select(bag=>bag.Color)
+                .ToList();
+            Console.WriteLine($"Result part 1: {resultStep1.Count()}");
+
+            //https://adventofcode.com/2020/day/7#part2
+            //2: How many individual bags are required inside your single shiny gold bag?
+            var resultStep2 = bagRules.Single(bagRule=>bagRule.Color == searchTerm).CountTotalBags(bagRules);
+            Console.WriteLine($"Result part 2: {resultStep2-1}"); // -1 för att den yttersta bagen inte ska räknas
         }
     }
 }
---
+++ AdventOfCode2020/Day07/bag.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Day07
+{
+    public class Bag
+    {
+        public string Color { get; private set; } = "";
+        private ICollection<(int, string)> _bagReferences = new List<(int, string)>();
+
+        private Bag() { }
+
+        public static Bag Map(string input)
+        {
+            var parse = input
+                .Replace("bags", "")
+                .Replace("bag", "")
+                .Trim('.')
+                .Split("contain", StringSplitOptions.RemoveEmptyEntries)
+                .Select(bag => bag.Trim())
+                .ToList();
+            return new Bag
+            {
+                Color = parse[0].Trim(),
+                _bagReferences = parse[1]
+                    .Split(",")
+                    .Select(bag => bag.Trim())
+                    .Select(bag => Regex.Split(bag, @"(?<=\d+)"))
+                    .Where(bag => bag.Length > 1)
+                    .Select(bag => (int.TryParse(bag[0], out var result) ? result : 0, bag[1].Trim()))
+                    .ToList()
+            };
+        }
+
+        public IEnumerable<Bag> Match(ICollection<Bag> allBags, string search)
+        {
+            if (!_bagReferences.Any())
+            {
+                return new List<Bag>();
+            }
+            if (_bagReferences.Select(bagReference => bagReference.Item2).Contains(search))
+            {
+                return new List<Bag>() { allBags.Single(bag => bag.Color == Color) };
+            }
+            return _bagReferences.SelectMany(bagReference => allBags
+                .Single(bag => bag.Color == bagReference.Item2)
+                .Match(allBags, search)).Any()
+                ? new List<Bag>() { allBags.Single(bag => bag.Color == Color) }
+                : new List<Bag>();
+        }
+
+        public int CountTotalBags(ICollection<Bag> allBags)
+        {
+            if (!_bagReferences.Any()) { return 1; }
+
+            return _bagReferences
+                .Select(bagReference =>
+                    bagReference.Item1 * allBags
+                        .Single(bag => bag.Color == bagReference.Item2)
+                        .CountTotalBags(allBags)).Sum()+1;
+        }
+    }
+}
